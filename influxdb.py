1201e7da8c850f3c25b240ff187ce78a72cee1e8
Kevin Hu <hukevin69@gmail.com>
2024-02-09 00:41:59 -0700

    using integration method to find the average power

--- influxdb.py
+++ influxdb.py
@@ -21,10 +21,29 @@ query_api = client.query_api()
 tables = query_api.query(query=query)
 
 measurement = []
+records = []
 for table in tables:
     for row in table.records:
         value = row.values["_value"] # row.values is a dictionary and _value is the key
         measurement.append(value)
+        record = [row.values["_time"], row.values["_value"]]
+        records.append(record) # integration method
+
+# Integration of Data
+t_intial = records[0][0].timestamp()
+for i in range(len(records)): # list modification
+    records[i][0] = records[i][0].timestamp() - t_intial
+
+area = 0
+for i in range(len(records)-1):
+    ts_i = records[i][0]
+    ts_f = records[i+1][0]
+    v_i = records[i][1]
+    v_f = records[i+1][1]
+    elapsed = ts_f - ts_i
+    area += (v_i * elapsed + (v_f - v_i) * elapsed / 2) / records[-1][0] # trapezoidal
+
+
 
 # Post Processing of Files
 c = 'C7' # C-State of the CPU
@@ -38,4 +57,4 @@ if not os.path.isfile('influxdb.csv'): # create file if it does not exist and wr
         csvfile.write('Timestamp, Hrs, Consumption, C-State\n')
 
 with open('influxdb.csv','a') as csvfile:
-    csvfile.write(f'{now}, {now_hrs}, {avg_consumption}, {c}\n')
+    csvfile.write(f'{now}, {now_hrs}, {area}, {c}\n')
